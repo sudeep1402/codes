3ae4d87338f7428b1e1f30580a9265f21c2d4539
sudeep <118767064+sudeep1402@users.noreply.github.com>
2024-12-22 15:47:27 +0530

    a3b2 -> aaabb

---
+++ compress.java
@@ -0,0 +1,23 @@
+public class compress {
+    public static String compress(String str) {
+      
+        StringBuilder new1=new StringBuilder("");
+        int count;
+        for (int i = 0; i <  str.length(); i++) {
+            if(Character.isDigit(str.charAt(i))){
+                count=str.charAt(i)-'0';
+                for (int j = 1; j <count; j++) {
+                    new1.append(str.charAt(i-1));
+                }
+            }
+            else{
+                    new1.append(str.charAt(i));
+                }           
+        }
+        return new1.toString();
+    }
+    public static void main(String[] args) {
+        String str="a3b2c5";
+        System.out.println("new string is:"+compress(str));    
+   }
+}
