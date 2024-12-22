7b7a86cd5c1a4b3b81f88bcbc68fe7174fc5ada5
sudeep <118767064+sudeep1402@users.noreply.github.com>
2024-12-22 23:19:13 +0530

    sumofdigits & reverse

---
+++ Sum_Of_Digit.java
@@ -0,0 +1,18 @@
+public class Sum_Of_Digit {
+    public static int sumofdigits(int x) {
+        int dig,rem=0;
+        while(x>0){
+            // dig=dig%10;
+            dig=x%10;
+            rem=rem+dig;
+            // for reverse of the given digits
+            // rem=rem*10+rem;
+            x=x/10;
+        }
+        return rem;
+    }
+    public static void main(String[] args) {
+        System.out.println("sum of digits are:"+sumofdigits(123));
+        
+    }
+}
