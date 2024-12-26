e5ad941135a00ebd02a6b9926e0c5b068768e703
sudeep <118767064+sudeep1402@users.noreply.github.com>
2024-12-27 00:13:33 +0530

    no code

---
+++ RotateArray.java
@@ -0,0 +1,16 @@
+public class RotateArray {
+    public static int[] rotate(int arr[],int k) {
+        int n=arr.length;
+                
+        for (int i = 0; i < n; i++) {
+            
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7};
+        int k=3;
+        int res[]=rotate(arr,k);
+        System.out.println("rotated array is : "+res.toString());
+        
+    }
+}
