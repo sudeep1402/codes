f6175e8187cb0757caa5f8a14bd9a10af0bbb610
sudeep <118767064+sudeep1402@users.noreply.github.com>
2024-12-27 23:50:33 +0530

    lstwrdlen

---
+++ LengthofLastWord.java
@@ -0,0 +1,17 @@
+public class LengthofLastWord {
+    public static void lastword(String str) {
+        int lstwrd=0;
+        String[] word= str.split(" ");
+        int n=word.length;
+        System.out.println(" "+word[n-1]);
+        String last=word[n-1];
+        System.out.println(""+last.length());
+    
+    }
+    public static void main(String[] args) {
+        String str="luffy is still joyboy";
+
+        lastword(str);
+        
+    }
+}
