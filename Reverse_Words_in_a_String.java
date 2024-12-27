0f94f61b1a8a1471d48bd4f74ab87cb72f3432c5
sudeep <118767064+sudeep1402@users.noreply.github.com>
2024-12-28 01:06:53 +0530

    updated

--- Reverse_Words_in_a_String.java
+++ Reverse_Words_in_a_String.java
@@ -4,7 +4,6 @@ public class Reverse_Words_in_a_String {
     public static void main(String[] args) {
         String str="a good   example";
         String[] word=str.split("\\s+");
-        
         int n =word.length;
         System.out.println(n);
         
