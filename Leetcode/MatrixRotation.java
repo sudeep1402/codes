f0945c961af5cd294c2ef162582d03bbf6660645
sudeep1402 <sudeepd2020@gmail.com>
2025-03-01 14:56:35 +0530

    matrix rotation without using extra space

---
+++ Leetcode/MatrixRotation.java
@@ -0,0 +1,54 @@
+
+public class MatrixRotation {
+
+    // Method to rotate the matrix 90 degrees in place
+    public static void rotate90DegreesInPlace(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+
+    // Method to print the matrix
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Input matrix (3x3 matrix for the example)
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        // Rotate the matrix 90 degrees in place
+        rotate90DegreesInPlace(matrix);
+
+        System.out.println("Matrix After 90 Degree Rotation:");
+        printMatrix(matrix);
+    }
+}
